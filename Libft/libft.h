b83b5687c4421930e2e7a83c10afdef6cfb3f928
Paulo Cesar Goncalves Nicolau <paugonca@c1r19s6.42lisboa.com>
2022-10-31 13:03:57 +0000

    Started implementing the necessary Libft functions.

---
+++ Libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 12:38:44 by paugonca          #+#    #+#             */
+/*   Updated: 2022/10/31 12:41:53 by paugonca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_itoa_base(unsigned long value, int base);
+
+#endif
