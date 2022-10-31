9aafe8ae463a6191e0d9c1f6a3f4c11b54746cf8
Paulo Cesar Goncalves Nicolau <paugonca@c1r19s6.42lisboa.com>
2022-10-31 14:39:13 +0000

    Completed Libft implemention for this project.

--- Libft/ft_itoa_base.c
+++ Libft/ft_itoa_base.c
@@ -6,15 +6,48 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:53:59 by paugonca          #+#    #+#             */
-/*   Updated: 2022/10/31 13:03:06 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/10/31 14:38:25 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int numlen(unsigned long num, int base)
+int	numlen(unsigned long num, int base)
 {
 	int	p;
 
-	i = 0;
+	p = 0;
+	if (num < 0)
+		num *= -1;
+	while (num > 0)
+	{
+		num /= base;
+		p++;
+	}
+	return (p);
+}
+
+char	*ft_itoa_base(unsigned long num, int base)
+{
+	int		p;
+	int		len;
+	char	*radix;
+	char	*res;
+
+	if (num == 0)
+		return (ft_strdup("0"));
+	p = 0;
+	len = numlen(num, base);
+	radix = "0123456789ABCDEF";
+	res = malloc(len + 1);
+	if (!res)
+		return (0);
+	res[len] = '\0';
+	while (len > 0)
+	{
+		res[len - 1] = radix[num % base];
+		num /= base;
+		len--;
+	}
+	return (res);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:38:44 by paugonca          #+#    #+#             */
-/*   Updated: 2022/10/31 12:41:53 by paugonca         ###   ########.fr       */
+/*   Updated: 2022/10/31 14:32:19 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ size_t	ft_strlen(const char *s);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_itoa_base(unsigned long value, int base);
+char	*ft_strdup(const char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
 
 #endif
